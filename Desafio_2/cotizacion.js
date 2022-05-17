1cd7ad9906435792327525956c6a6fbc73643c65
Lorenzo <maximo.vieyra@gmail.com>
2022-05-17 20:03:56 -0300

    Corregir valores de comision

--- Desafio_2/cotizacion.js
+++ Desafio_2/cotizacion.js
@@ -1,6 +1,6 @@
 /*Un negocio se dedica a la venta de computadoras. El vendedor tiene un sueldo mensual pero además de su sueldo 
 gana una comisión por venta. * Si el monto de venta es mayor de 10000 la comisión será del 20%. * Si el monto de 
-venta es mayor o igual a 5000 y menor de 1000 la comisión es del 10%. * Si el monto de la venta es menor que 1000 
+venta es mayor o igual a 5000 y mayor o igual  de 1000 la comisión es del 10%. * Si el monto de la venta es menor que 1000 
 no tiene comisión.
 */
 const comision = (monto, porcentaje) => monto * porcentaje;
@@ -21,7 +21,7 @@ function calcularSueldos(vendedor){
     }while(isNaN(sueldo) || sueldo<0 || isNaN(montoVentas) || montoVentas < 0)
         if(montoVentas> 10000){
            miComision= comision(montoVentas, 0.20); 
-        }else if (montoVentas >=5000 || montoVentas<1000) {
+        }else if (montoVentas >=5000 || montoVentas>=1000) {
             miComision = comision(montoVentas, 0.10)
         }else{
             alert("No cobra comisión");
